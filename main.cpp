0356e9a09357d6cf2105f2f63614e219b3d76967
Joshua Cogburn <joshua.dfw86@gmail.com>
2016-08-22 04:09:30 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: master_yoda
+ *
+ * Created on December 22, 2014, 10:11 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"Hello World";
+    return 0;
+}
+
